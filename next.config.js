4365e1c57a01a23187a5e9be4013953f64b8f906
maul21 <bintang1350@gmail.com>
2026-03-11 00:43:43 +0700

    Abaikan error ESLint saat build di Vercel

--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          // Proxy all /api/* requests to your real backend
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/:path*`
-        }
-      ]
-    }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        // Proxy all /api/* requests to your real backend
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/:path*`
+      }
+    ]
   }
+}
   
   module.exports = nextConfig
   
